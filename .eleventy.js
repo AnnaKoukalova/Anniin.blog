0a0bdb29b0ed20fc4b9d00074ea78e5b8e0e2886
AnnaKoukalova <145343747+AnnaKoukalova@users.noreply.github.com>
2023-12-22 12:04:30 +0100

    2

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,17 @@
-module.exports = function(eleventyConfig) {
-    // Výchozí výstupní složka je: _site
-    // Zkopírovat images/ do _site/images
-    eleventyConfig.addPassthroughCopy("images");
-    // Zkopírovat css/ to _site/css/
-    eleventyConfig.addPassthroughCopy("css");
-   };
-   module.exports = function(eleventyConfig) {
-    // ...necháme, co už v konfiguraci je, a přidáme:
-    return {
+module.exports = function (eleventyConfig) {
+  // Výchozí výstupní složka je: _site
+  // Zkopírovat images/ do _site/images
+  eleventyConfig.addPassthroughCopy("image");
+  // Zkopírovat css/ to _site/css/
+  eleventyConfig.addPassthroughCopy("css");
+ 
+  return {
       // možné formáty šablon
       templateFormats: ["njk", "html", "md"],
       // jako šablonovací jazyk zvolíme Nunjucks`
       markdownTemplateEngine: "njk",
       htmlTemplateEngine: "njk",
       dataTemplateEngine: "njk",
-    }
-  };
-  
+  }
+
+};
